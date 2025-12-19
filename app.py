6afc5fe15cf552e5b1ce249429a2530606f5ae15
unknown <swetasaro@gmail.com>
2025-12-19 15:02:37 +0530

    Performed date parsing logic

--- app.py
+++ app.py
@@ -2,17 +2,28 @@ from flask import Flask, render_template
 import csv
 from database import create_tables
 import sqlite3
-create_tables()
 db_name='vehicle.db'
 app=Flask(__name__)
 
 
-def parse_data(file):
+def parse_data():
     cleaned_rows=[]
-    with open(file,'r') as f:
+    with open('driver_logs.csv','r') as f:
         readerObject=csv.DictReader(f)
         for row in readerObject:
             try:
+                date=row["Date"].strip()
+                if '/' in date:
+                    correct_date=date.split('/')
+                if '-' in date:
+                    correct_date=date.split('-')
+                for k in range(len(correct_date)):
+                    try:
+                        correct_date[k]=int(correct_date[k])
+
+                    except:
+                        continue
+                dt=correct_date.join('/')
                 row["Odometer_Reading"]=int(row["Odometer_Reading"])
                 row["Trip_Distance"]=int(row(["Trip_Distance"]))
                 cleaned_rows.append(row)
@@ -21,7 +32,8 @@ def parse_data(file):
     conn=sqlite3.connect(db_name)
     cur=conn.cursor()
     for row in cleaned_rows:
-        cur.execute('Insert into trips(VehicleID,DriverName,StartDate,DistanceKm) values(?,?,?,?)',(row['VehicleID'],row['Driver'],row['Odometer_Reading'],row['Trip_Distance']))
+        cur.execute('Insert into trips(VehicleID,StartDate,EndDate,DriverName,StartDate,DistanceKm) values(?,?,?,?,?,?)',(row['VehicleID'],dt,dt,row['Driver'],row['Odometer_Reading'],row['Trip_Distance']))
+    conn.commit()
 
 @app.route('/')
 def index():
