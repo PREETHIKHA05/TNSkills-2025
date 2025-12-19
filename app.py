5422b9385bcac48f170b339dfdf07728baa56df4
unknown <swetasaro@gmail.com>
2025-12-19 16:06:08 +0530

    Implemented backend logic for checking available vehicles

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 from database import create_tables
 import sqlite3
@@ -41,7 +41,20 @@ def show_tables():
      print(data)
 
 
-@app.route('/')
+@app.route('/check_availability',methods=['GET','POST'])
+def check_availability():
+     if request.method=='POST':
+          start_date=request.form.get('start_date')
+          end_date=request.form.get('end_date')
+          conn=sqlite3.connect(db_name)
+          cur=conn.cursor()
+          cur.execute('Select vehicles.vehicleID,vehicles.StartDate,vehicles.EndDate from vehicles,trip vehicles.VehicleID=trip.VehicleID',(start_date,end_date))
+          available=cur.fetchall()
+          vehicles_avail=[]
+          for id,s,e in available:
+               if e>end_date and s<start_date:
+                    vehicles_avail.append(id)
+          return render_template('check_availability.html',(vehicles_avail,))
 @app.route('/')
 def index():
     return render_template('index.html')
